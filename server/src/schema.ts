9eeae123437308ab6b63ae1749f8d1b9bdfa9fc4
Joao-Pedro-P-Holanda <joaopedroph.dev@gmail.com>
2024-03-31 17:55:51 -0300

    Lift-off 4 - Adding a mutation to our schema

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -8,6 +8,17 @@ export const typeDefs = gql`
     track(id: ID!): Track
   }
 
+  type Mutation {
+    incrementTrackViews(id: ID!): IncrementedTrackViewsResponse
+  }
+
+  type IncrementTrackViewsResponse {
+    code: Int!
+    sucess: Boolean!
+    response: String!
+    track: Track
+  }
+
   "A track is a group of Modules that teaches about a specific topic"
   type Track {
     id: ID!
